54109a6eac175e9a56f081c3421d44631d7798c3
Franco Mp <stha-182@hotmail.com>
2019-09-13 16:04:48 -0500

    2019/09/13 16:04 -l

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import P_Alg_Lin from '../files/Algebra_Lineal_Prac3_10p.pdf';
+
+/* Links */
+import Alg0 from '../files/Algebra_Lineal_Prac3_10p.pdf';
+import Cri1 from '../files/Cripto_EJER1.pdf';
+/* Links */
 
 class Body extends React.Component {
 
@@ -14,16 +18,33 @@ class Body extends React.Component {
   render() {
     return (
       <div className="App-body">
-        <div className="list-group">
-          <div className="list-group-item list-group-item-action">
-            <div className="d-flex w-100 justify-content-between">
-              <h4><a href={P_Alg_Lin} className="badge" download>Algebra_Lineal_Prac3_10p</a></h4>
-              <small>3 days ago</small>
+
+        <div className="list-group">                  
+                  
+        <input type='hidden'/>
+
+           <div className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h4><a href={Alg0} className="badge" download>Algebra_Lineal_Prac3_10p.pdf</a></h4>
+                <small>X days ago</small>
+              </div>
+              <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small>Donec id elit non mi porta.</small>
             </div>
-            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-            <small>Donec id elit non mi porta.</small>
-          </div>
+            
+           <div className="list-group-item list-group-item-action">
+              <div className="d-flex w-100 justify-content-between">
+                <h4><a href={Cri1} className="badge" download>Cripto_EJER1.pdf</a></h4>
+                <small>X days ago</small>
+              </div>
+              <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+              <small>Donec id elit non mi porta.</small>
+            </div>
+            
+        <input type='hidden'/>
+
         </div>
+
       </div>
     );
   }
